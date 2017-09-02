a05719ca9225bac70979a3097b54d3197715f356
Hussain Alsalman <Alsalman.hn@gmail.com>
2017-09-03 01:27:05 +0300

    Working with the example 1.4.2 Unemployment: Maine

--- All codes.R
+++ All codes.R
@@ -13,3 +13,32 @@ layout(1:2)
 plot(aggregate(AP))
 boxplot(AP ~ cycle(AP))
 
+# 1.4.2 Unemployment: Maine
+
+# Make sure you have set your working directory to where your files are
+www <- "Maine.dat"
+Maine.month <- read.table(www, header = TRUE)
+attach(Maine.month)
+class(Maine.month)
+
+Maine.month.ts <- ts(unemploy, start = c(1996, 1), freq = 12)
+Maine.annual.ts <- aggregate(Maine.month.ts)/12
+layout(1:2)
+plot(Maine.month.ts, ylab = "unemployed (%)")
+plot(Maine.annual.ts, ylab = "unemployed (%)")
+
+Maine.Feb <- window(Maine.month.ts, start = c(1996,2), freq = TRUE)
+Maine.Aug <- window(Maine.month.ts, start = c(1996,8), freq = TRUE)
+Feb.ratio <- mean(Maine.Feb) / mean(Maine.month.ts)
+Aug.ratio <- mean(Maine.Aug) / mean(Maine.month.ts)
+
+Feb.ratio
+
+Aug.ratio
+
+www <- "USunemp.dat"
+US.month <- read.table(www, header = T)
+attach(US.month)
+US.month.ts <- ts(USun, start=c(1996,1), end=c(2006,10), freq = 12)
+layout(1:1)
+plot(US.month.ts, ylab = "unemployed (%)")
